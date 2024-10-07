20362cbc7dc97a2f48def770265e9fc1fc31a569
Ansuman Gautam <ansumangautam2002@gmail.com>
2024-10-07 11:49:32 +0530

    Add files via upload

---
+++ ansuman_react/frontend/javascript/class1.js
@@ -0,0 +1,12 @@
+let a = 2;
+let b = 3;
+let addition = a + b;
+let sub = a - b;
+let multi = a * b;
+let div= a / b;
+
+
+console.log(addition);
+console.log(sub);
+console.log(multi);
+console.log(div);
