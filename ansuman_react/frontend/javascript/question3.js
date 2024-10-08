ec331cc7d384cd9366be731fe6300ba02752c825
Ansuman Gautam <ansumangautam2002@gmail.com>
2024-10-08 11:16:52 +0530

    Add files via upload

---
+++ ansuman_react/frontend/javascript/question3.js
@@ -0,0 +1,13 @@
+// TODO:
+// Question-3:
+// Write a JavaScript program that calculates the total amount a customer needs to pay after adding sales tax to the product price. The program should:
+
+// Define a variable productPrice for the cost of the product.
+// Define a variable salesTaxRate to store the sales tax percentage (e.g., 8%).
+// Calculate the tax amount and the total price (product price + tax).
+// Display the total price after adding sales tax using console.log.
+
+let productPrice= 100;
+let salesTaxRate= 100 *(8/100);
+let total = productPrice+salesTaxRate;
+console.log("tax rate = ", total);
