09c49979789bda12000685d92f77aa2f4907dcf2
Ansuman Gautam <ansumangautam2002@gmail.com>
2024-10-23 10:36:17 +0530

    Add files via upload

---
+++ ansuman_react/frontend/javascript/functionn/q12.js
@@ -0,0 +1,21 @@
+// TODO:to gret from user
+const greetUser = (name) => {
+    const currentHour = new Date().getHours();
+    let greeting;
+  
+    if (currentHour < 12) {
+      greeting = "Good Morning";
+    } else if (currentHour < 17) {
+      greeting = "Good Afternoon";
+    } else if (currentHour < 21) {
+      greeting = "Good Evening";
+    } else {
+      greeting = "Good Night";
+    }
+  
+    console.log(greeting +  " "  +name);
+  };
+  
+ 
+  greetUser("ansuman");
+  
