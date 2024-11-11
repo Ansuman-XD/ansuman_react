5f925748de08a1c9635e3c9bd44d2cd34fb1b86a
Ansuman Gautam <ansumangautam2002@gmail.com>
2024-11-11 12:31:11 +0530

    Add files via upload

---
+++ ansuman_react/frontend/javascript/array/api.js
@@ -0,0 +1,32 @@
+// TODO:create ann array of object of 50 mca students
+
+// [{},{},{},{}]
+
+const students = [
+    { regNumber: "2306274145", name: "Sushree Sonita Bilwal", topic: "React" },
+    { regNumber: "2315274047", name: "Jayashree Rous", topic: "React" },
+    { regNumber: "2305274030", name: "Chinmayer Sahou", topic: "React" },
+    { regNumber: "2305274056", name: "Madhusmita Gouda", topic: "React" },
+    { regNumber: "2305274076", name: "Rujanini Das", topic: "React" },
+    { regNumber: "2305274082", name: "Rome Mohapatra", topic: "React" },
+    { regNumber: "2305274092", name: "Camechaha Legka", topic: "React" },
+    { regNumber: "2205274090", name: "Samanwita Pattaraph", topic: "React" },
+    { regNumber: "2305274072", name: "Prealikinya Pr. Mohanty", topic: "React" },
+    { regNumber: "2305274057", name: "Madhusmita Nayah", topic: "React" },
+    { regNumber: "2305274064", name: "Monali Barcik", topic: "React" },
+    { regNumber: "2305274181", name: "Soumyashree Beherea", topic: "React" },
+    { regNumber: "2305274127", name: "Sabhalaxmi Das", topic: "React" },
+    { regNumber: "2305274146", name: "Susmita Sahani", topic: "React" },
+    { regNumber: "2305274002", name: "Abhipsa Das Adhikari", topic: "React" },
+    { regNumber: "2305274012", name: "Anshita Rout", topic: "React" },
+    { regNumber: "2305274052", name: "Unnamed Student", topic: "React" },
+    { regNumber: "2305274109", name: "Unnamed Student", topic: "React" },
+    { regNumber: "2305274136", name: "Unnamed Student", topic: "React" },
+    { regNumber: "2305274065", name: "Unnamed Student", topic: "React" },
+    { regNumber: "2305274019", name: "Unnamed Student", topic: "React" },
+    { regNumber: "2305274003", name: "Lipsa Mabasty", topic: "React" },
+    { regNumber: "2305274117", name: "Soumya Renian Des", topic: "React" },
+    { regNumber: "250527126", name: "Soumyarenton", topic: "React" },
+    
+  ];
+  
